9f7851b76b50a7ffeb1b6de96da1ba6e6246f741
Camana Nick <camananick@gmail.com>
2026-03-24 09:39:13 -0500

    Bloque B — estructura de carpetas del restaurante creada

---
+++ Dia2/restaurante-frontend/src/components/PlatoCard.jsx
@@ -0,0 +1,15 @@
+const PlatoCard = ({ plato }) => {
+    return (
+        <div style={{ border: "1px solid gray", margin: "10px", padding: "10px" }}>
+            <h3>{plato.nombre}</h3>
+            <p>Categoría: {plato.categoria}</p>
+            <p>Precio: S/ {plato.precio}</p>
+            <p>Stock: {plato.stock}</p>
+            <p>
+                {plato.disponible ? "Disponible" : "Agotado"}
+            </p>
+        </div>
+    )
+}
+
+export default PlatoCard
