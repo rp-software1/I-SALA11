195aa05d8c55cb0470cd91a65cfb688f26320b1e
Camana Nick <camananick@gmail.com>
2026-03-25 12:59:29 -0500

    NavBar.jsx Actualizado

--- Restaurante-App/restaurante-frontend/src/components/NavBar.jsx
+++ Restaurante-App/restaurante-frontend/src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 const NavBar = ({ nombreRestaurante = "Restaurante" }) => {
     return (
-        <nav style={{ backgroundColor: "#eee", padding: "15px", marginBottom: "20px" }}>
+        <nav style={{ backgroundColor: "#8f5353ff", padding: "15px", marginBottom: "20px" }}>
             <h2>{nombreRestaurante}</h2>
             <p>Carta | Mesas | Comandas</p>
         </nav>
