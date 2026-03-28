5b1571226273434eab908ad7b706ed1c918cd753
Camana Nick <camananick@gmail.com>
2026-03-28 08:19:44 -0500

    Actualizar estilos en NavBar

--- Restaurante-App/restaurante-frontend/src/components/NavBar.jsx
+++ Restaurante-App/restaurante-frontend/src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 const NavBar = ({ nombreRestaurante = "Restaurante" }) => {
     return (
-        <nav style={{ backgroundColor: "#8f5353ff", padding: "15px", marginBottom: "20px" }}>
+        <nav style={{ backgroundColor: "#b5b8c4ff", padding: "15px", marginBottom: "20px" }}>
             <h2>{nombreRestaurante}</h2>
             <p>Carta | Mesas | Comandas</p>
         </nav>
