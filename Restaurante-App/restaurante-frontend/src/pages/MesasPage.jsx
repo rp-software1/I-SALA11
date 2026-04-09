1c1edd799d3384fb626107061ba0843ef7bf2d64
Camana Nick <camananick@gmail.com>
2026-04-09 13:19:25 -0500

    React Dia7 Bloque A — React Router instalado rutas Menu Mesas Carrito

--- Restaurante-App/restaurante-frontend/src/pages/MesasPage.jsx
+++ Restaurante-App/restaurante-frontend/src/pages/MesasPage.jsx
@@ -1,4 +1,4 @@
-import MesaCard from "../components/MesaCard";
+/*import MesaCard from "../components/MesaCard";
 import { mesasMock } from "../data/mesas.mock";
 
 const MesasPage = () => {
@@ -19,4 +19,33 @@ const MesasPage = () => {
     );
 };
 
-export default MesasPage;
+export default MesasPage;*/
+
+// DIA 7
+import { Link } from "react-router-dom";
+import { mesasMock } from "../data/mesas.mock";
+
+export default function MesasPage() {
+    return (
+        <div style={{ padding: "20px" }}>
+            <h1>Mesas del Restaurante</h1>
+
+            {mesasMock.map((mesa) => (
+                <div
+                    key={mesa.id}
+                    style={{
+                        border: "1px solid gray",
+                        padding: "10px",
+                        marginBottom: "10px",
+                    }}
+                >
+                    <h3>Mesa {mesa.numero}</h3>
+                    <p>Capacidad: {mesa.capacidad}</p>
+                    <p>Estado: {mesa.disponible ? "Disponible" : "Ocupada"}</p>
+
+                    <Link to={`/mesas/${mesa.id}`}>Ver detalle</Link>
+                </div>
+            ))}
+        </div>
+    );
+}
