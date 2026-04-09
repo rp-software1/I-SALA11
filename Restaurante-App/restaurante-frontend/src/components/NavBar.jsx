1d80e1cfd667584b206891184d9ddf639c678d4d
Camana Nick <camananick@gmail.com>
2026-04-09 13:19:50 -0500

    React Dia7 Bloque B — NavBar con NavLink y estado activo

--- Restaurante-App/restaurante-frontend/src/components/NavBar.jsx
+++ Restaurante-App/restaurante-frontend/src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import PropTypes from "prop-types";
+/*import PropTypes from "prop-types";
 
 const NavBar = ({ nombreRestaurante = "Restaurante" }) => {
     return (
@@ -13,4 +13,33 @@ NavBar.propTypes = {
     nombreRestaurante: PropTypes.string,
 };
 
-export default NavBar;
+export default NavBar;*/
+
+import { NavLink } from "react-router-dom";
+
+export default function NavBar({ nombreRestaurante }) {
+    const estiloLink = ({ isActive }) => ({
+        marginRight: "15px",
+        textDecoration: "none",
+        fontWeight: isActive ? "bold" : "normal",
+        color: isActive ? "orange" : "black",
+    });
+
+    return (
+        <nav style={{ backgroundColor: "#d3d3dd", padding: "20px" }}>
+            <h1>{nombreRestaurante}</h1>
+
+            <NavLink to="/" style={estiloLink}>
+                Carta
+            </NavLink>
+
+            <NavLink to="/mesas" style={estiloLink}>
+                Mesas
+            </NavLink>
+
+            <NavLink to="/carrito" style={estiloLink}>
+                Comandas
+            </NavLink>
+        </nav>
+    );
+}
